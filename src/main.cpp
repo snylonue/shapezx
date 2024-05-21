f9d20775510ff73d0b19c5a38ff4fff1d24ce435
snylonue <snylonue@gmail.com>
2024-05-21 20:58:21 +0800

    introduce slint

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,10 @@
-#include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
-
-#include <memory>
+#include "ui.h"
  
-int main(int argc, char **argv) {
-  auto window = std::make_unique<Fl_Window>(340,180);
-  auto box = std::make_unique<Fl_Box>(20,40,300,100,"Hello, World!");
-  box->box(FL_UP_BOX);
-  box->labelfont(FL_BOLD+FL_ITALIC);
-  box->labelsize(36);
-  box->labeltype(FL_SHADOW_LABEL);
-  window->end();
-  window->show(argc, argv);
-  return Fl::run();
+int main(int argc, char **argv)
+{
+    auto hello_world = HelloWorld::create();
+    hello_world->set_my_label("Hello from C++");
+    // Show the window and spin the event loop until the window is closed.
+    hello_world->run();
+    return 0;
 }
