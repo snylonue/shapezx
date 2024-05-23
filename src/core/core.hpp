4bddae65116f89f06884855eab787b182a805004
snylonue <snylonue@gmail.com>
2024-05-23 21:55:03 +0800

    build: use c++23
    and fmt code

--- src/core/core.hpp
+++ src/core/core.hpp
@@ -8,23 +8,19 @@
 
 namespace shapezx {
 
+using std::nullopt;
+using std::size_t;
 using std::unique_ptr;
 using std::vector;
-using std::size_t;
-using std::nullopt;
 
 struct Chunk {
   optional<Ore> ore;
   optional<unique_ptr<Building>> building;
-  
+
   Chunk() = default;
-  Chunk(const Chunk &chunk): ore(chunk.ore) {
-    if (chunk.building.has_value()) {
-      this->building = chunk.building.value()->clone();
-    } else {
-      this->building = nullopt;
-    }
-  }
+  Chunk(const Chunk &chunk)
+      : ore(chunk.ore), building(chunk.building.transform(
+                            [](const auto &b) { return b->clone(); })) {}
 };
 
 struct Map {
@@ -33,7 +29,12 @@ struct Map {
   size_t width;
 
   // caller must guarantee that chks.size() == h * w
-  Map(const vector<Chunk>& chks, size_t h, size_t w): chunks(chks), height(h), width(w) {}
-  Map(size_t h, size_t w): chunks(h * w), height(h), width(w) {} 
+  Map(const vector<Chunk> &chks, size_t h, size_t w)
+      : chunks(chks), height(h), width(w) {}
+  Map(size_t h, size_t w) : chunks(h * w), height(h), width(w) {}
+};
+
+struct State {
+  Map map;
 };
 } // namespace shapezx
