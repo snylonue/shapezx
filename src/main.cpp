73d3bf92bd2621cc04ff039ef9365f559287e399
snylonue <snylonue@gmail.com>
2024-06-02 23:33:23 +0800

    ui: introduce SurealismUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
-#include "ui.h"
+#include "app.h"
  
+#include "core/core.hpp"
+
 int main(int argc, char **argv)
 {
+    auto core = shapezx::Map{{shapezx::Chunk{}}, 1, 1};
+
     auto hello_world = HelloWorld::create();
     hello_world->set_my_label("Hello from C++");
     // Show the window and spin the event loop until the window is closed.
